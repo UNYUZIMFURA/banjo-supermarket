77d7d799262db8769ac555540fc24edaabb623c1
Nshuti Alliance <63238512+alliancenshuti@users.noreply.github.com>
2022-10-05 14:24:54 +0200

    Update Main.java
    removed unnecessary printing line and closed the scanner

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,11 @@ public class Main {
             int initialAmount = scanner.nextInt();
             System.out.println("");
             System.out.println("-------- List of products --------");
-            System.out.println("1.Fried Chips: $30");
-            System.out.println("2.Mutziig: $20");
-            System.out.println("3.Ice cream: $5");
-            System.out.println("4.Pizza: $4");
-            System.out.println("5.Yoghurt: $3");
-            System.out.println("6.Bread: $2");
-            System.out.println("");
+            System.out.println("1.Fried Chips: $30\n2.Mutziig: $20\n3.Ice cream: $5\n4.Pizza: $4\n5.Yoghurt: $3\n6.Bread: $2\n");
             System.out.println("How many products do you want to buy? ");
             int productNumber = scanner.nextInt();
             System.out.println("Buy Product(s) by choosing its/their number!: ");
+            scanner.close();
             // Testing jere
             for (int i = 1; i <= productNumber; i++) {
                 System.out.println("Enter product " + i);
